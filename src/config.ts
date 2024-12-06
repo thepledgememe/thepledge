d75532f112d6c5720be933daa53391466fb622e6
Vladimir <vladimirhovsepyan@gmail.com>
2024-12-07 03:23:32 +0400

    Fix whitepaper link

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,17 @@
 const CONFIG = {
-    LINKS: {
-        UNISWAP: "https://app.uniswap.org/",
-        DISCORD: "https://discord.com/invite/vHsp8sukvH",
-        PDF: "../../public/PledgeWhitepaper.pdf",
-        PLEDGE: "/your-pledge",
-        TELEGRAM: "https://t.me/the_pledge_channel",
-        INSTAGRAM: "https://www.instagram.com/thepledgememe",
-        X: "https://x.com/thepledgememe?mx=2",
-        DEX_TOOLS: "https://www.dextools.io/app/en/ether/pair-explorer/0xc3a4f2231453535fa564508eb6828bd449705082",
-        DEX_SCREENER: "https://dexscreener.com/ethereum/0xc3a4f2231453535fa564508eb6828bd449705082",
-    },
+  LINKS: {
+    UNISWAP: "https://app.uniswap.org/",
+    DISCORD: "https://discord.com/invite/vHsp8sukvH",
+    PDF: "/PledgeWhitepaper.pdf",
+    PLEDGE: "/your-pledge",
+    TELEGRAM: "https://t.me/the_pledge_channel",
+    INSTAGRAM: "https://www.instagram.com/thepledgememe",
+    X: "https://x.com/thepledgememe?mx=2",
+    DEX_TOOLS:
+      "https://www.dextools.io/app/en/ether/pair-explorer/0xc3a4f2231453535fa564508eb6828bd449705082",
+    DEX_SCREENER:
+      "https://dexscreener.com/ethereum/0xc3a4f2231453535fa564508eb6828bd449705082",
+  },
 };
 
 export default CONFIG;
